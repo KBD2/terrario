e7143516ea9e241b09ed84f2d017cded48bfe1c0
KBD2 <44832995+KBD2@users.noreply.github.com>
2020-07-05 00:47:54 +1200

    Grayscale test
    Screen is filled with stone tiles

--- src/main.c
+++ src/main.c
@@ -1,12 +1,24 @@
 #include <gint/display.h>
 #include <gint/keyboard.h>
+#include <gint/gray.h>
 
 int main(void)
 {
-	dclear(C_WHITE);
-	dtext(1, 1, "Sample fxSDK add-in.", C_BLACK, C_NONE);
-	dupdate();
+	extern image_t img_tile_stone;
+
+	gray_start();
+	gclear(C_WHITE);
+	for(int y = 0; y < 64; y += 8)
+	{
+		for(int x = 0; x < 128; x += 8)
+		{
+			gimage(x, y, &img_tile_stone);
+		}
+	}
+	gupdate();
 
 	getkey();
+	gray_stop();
+
 	return 1;
 }
